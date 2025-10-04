8d6b2e97da72290e55bee995099b1fcfee641a86
Kirito2780 <antonparshuk388@gmail.com>
2025-10-04 22:39:34 +0300

    half of register form

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import LogInPage from "../Pages/Log In Page/LogInPage.tsx";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="App">
+      <LogInPage />
+    </main>
+  );
+}
+
+export default App;
